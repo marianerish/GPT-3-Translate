d46b3fe2f056a0eee5fa12fa9c7fc25be801984e
marianerish <101545988+marianerish@users.noreply.github.com>
2022-04-05 02:47:27 -0400

    openai import changed

--- utils/translate.ts
+++ utils/translate.ts
@@ -1,8 +1,8 @@
 import UserAgent from "user-agents";
 import cheerio from "cheerio";
 import { replaceBoth, LangCode } from "./language";
-import { Configuration, OpenAIApi } from "openai";
 
+const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({
     apiKey: "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"
 });
